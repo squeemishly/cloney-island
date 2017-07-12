7294e9e855f35792583dbbd5f83f3a69cd075b9b
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-12 07:23:32 -0600

    Combine guest visits root and guest navigates root tests

--- spec/features/guest_visits_root_spec.rb
+++ spec/features/guest_visits_root_spec.rb
@@ -34,4 +34,27 @@ feature "guest visits root" do
       expect(page).to have_selector(".column-list")
     end
   end
+
+  context "they click on a city" do
+    it "they see the display page for the city they clicked on" do
+      visit root_path
+
+      within ('.column-list') do
+        expect(page).to have_selector(".city-preview")
+        click_on "Paris, France"
+      end
+
+      # We expect the map to center on Paris (or whatever city we click), but it's
+      # Javascript. I don't think capybara can test for javascript
+
+      within(".column-list") do
+        expect(page).to have_content("Paris")
+        expect(page).to_not have_content("London")
+        expect(page).to have_selector(".place-preview")
+      end
+
+      # We expect the path to change to show the lat and long coordinates
+      # I don't know how to test for that -- it might be more of a service test?
+    end
+  end
 end
