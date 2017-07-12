2d5e06abb6b79e0c2b0d8103223f8ad319858f72
NataliaColome <nmcolome@gmail.com>
2017-07-11 20:12:16 -0600

    add feature test for admin and vendor functionality

---
+++ spec/features/admin/admin_dashboard_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+Rspec.describe "Admin" do
+  before :each do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    admin = create(:user)
+    role = create(:role, name: "admin")
+    admin.roles << role
+  end
+
+  it "sees their dashboard" do
+    expect(current_path).to eq("/admin/#{admin.id}/dashboard")
+
+    expect(page).to have_selector('.search')
+  end
+end
+
+# As an admin
+# When I login, 
+# I am redirected to /admin/dashboard
+# And I see user search field
+# * Maybe add analytics later?
