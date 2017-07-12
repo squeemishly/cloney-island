6a987fe0f6a3bd7bdc107aebfd4dada4f45b70e2
squeemishly <squeemishly@gmail.com>
2017-07-12 11:53:01 -0600

    model tests started for each model

--- spec/models/itinerary_spec.rb
+++ spec/models/itinerary_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Itinerary do
   context "attributes" do
-
+    it { should validate_presence_of(:trip_id) }
+    it { should validate_presence_of(:place_id) }
+    it { should validate_presence_of(:date) }
   end
 
   context "relationships" do
-
+    itinerary = create(:itinerary)
+    expect(itinerary).to respond_to :trips
+    expect(itinerary).to respond_to :places
   end
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Trip do
   context "attributes" do
-
+    it { should validate_presence_of(:start_date) }
+    it { should validate_presence_of(:end_date) }
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:start_city) }
   end
 
   context "relationships" do
-
+    trip = create(:trip)
+    expect(trip).to respond_to :users
+    expect(trip).to respond_to :itineraries
+    expect(trip).to respond_to :cities
   end
 end
