04756fe3044c5d615340aec72f33e36e98dd3cc1
rwilhel <rwilhel@gmail.com>
2017-07-11 22:11:18 -0600

    Adds feature tests for user viewing trips from navbar

---
+++ spec/features/user_can_view_list_of_trips.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "A signed-in user view a list of their planned trips" do
+  it "can view a list of planned trips" do
+    user = create(:user)
+    trip1 = create(:trip)
+    trip2 = create(:trip)
+
+    visit root_path
+
+    click_on 'Sign In Using Email'
+    fill_in 'Email', with: "#{user.email}"
+    fill_in 'Password', with: 'password'
+    click_on "Sign in"
+    select('My Trips', :from => 'Select Box')
+
+    expect(current_path).to eq(trips_path)
+    expect(page).to have_content("My Trips")
+    expect(page).to have_content(trip1.title)
+    expect(page).to have_content(trip1.start_date)
+    expect(page).to have_content(trip1.end_date)
+    expect(page).to have_content(trip1.start_city)
+    expect(page).to have_content(trip2.title)
+    expect(page).to have_content(trip2.start_date)
+    expect(page).to have_content(trip2.end_date)
+    expect(page).to have_content(trip2.start_city)
+
+    click_on "#{trip1.name}"
+    expect(current_path).to eq(trip_path)
+  end
+end
