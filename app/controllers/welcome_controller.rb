2a4a7621ff8ed0b1bfba3b391773f1fbd4e2fe09
Sarah Kirk <kirk710@gmail.com>
2017-07-13 13:06:09 -0600

    added functionality to pass in coordiates from the welcome page to js

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    @cities = [{name: "London", description: "Has a clock"},
-              {name: "Paris", description: "Has a metal tower"},
-              {name: "Denver", description: "Has a scary horse"}]
+    @cities = [{name: "London", country:"England", description: "Has a clock", coordinates:{lat: 51.5074, lng: 0.1278}},
+              {name: "Paris", country:"France", description: "Has a metal tower", coordinates:{lat: 48.8566, lng: 2.3522}},
+              {name: "Denver", country: "United States", description: "Has a scary horse", coordinates:{lat: 39.7392, lng: -104.9903}}]
   end
 end
