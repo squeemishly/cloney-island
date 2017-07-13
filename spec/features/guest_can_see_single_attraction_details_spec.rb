58569e752d1374656c0349094f8cdb0d13278f4c
Sarah Kirk <kirk710@gmail.com>
2017-07-13 16:56:34 -0600

    added VCR to guest_can_see_single_attraction test, and add CSS selector to h2 in search index

--- spec/features/guest_can_see_single_attraction_details_spec.rb
+++ spec/features/guest_can_see_single_attraction_details_spec.rb
@@ -3,45 +3,47 @@ require 'rails_helper'
 feature "guest navigates to individual attraction from city" do
   context "they are not logged in" do
     it "they see a the details of the attraction they clicked on" do
-      visit root_path
+      VCR.use_cassette("single_attraction") do
+        visit root_path
 
-      within ('.column-list') do
-        expect(page).to have_selector(".city-preview")
-        click_on "Paris, France"
-      end
+        within ('.column-list') do
+          expect(page).to have_selector(".city-preview")
+          click_on "Paris, France"
+        end
 
-      within first(".place-preview") do
-        page.find(".place-preview-name").click
-      end
+        within first(".attraction-preview") do
+          page.find(".attraction-preview-name").click
+        end
 
-      #Once again, we expect the map to change, to zoom in on the location of the
-      # attraction. Not sure if we can test this, but if we can, it might be through
-      # services rather than capybara features
+        #Once again, we expect the map to change, to zoom in on the location of the
+        # attraction. Not sure if we can test this, but if we can, it might be through
+        # services rather than capybara features
 
-      within ('.column-list') do
-        expect(page).to have_selector(".add-attraction-button")
-        expect(page).to have_selector(".city-details")
-        expect(page).to have_selector(".city-details-img")
-        expect(page).to have_selector(".city-details-name")
-        expect(page).to have_selector(".city-details-description")
-        expect(page).to have_selector(".city-details-wiki")
-        expect(page).to have_content("Wikipedia Article")
-        expect(page).to have_selector(".city-details-website")
-        expect(page).to have_content("Official Website")
-        expect(page).to have_selector(".city-details-admission")
-        expect(page).to have_content("Admission")
-        expect(page).to have_selector(".city-details-hours")
-        expect(page).to have_content("Opening Hours")
-        expect(page).to have_selector(".city-details-address")
-        expect(page).to have_content("Address")
-        expect(page).to have_selector(".city-details-phone")
-        expect(page).to have_selector(".city-details-email")
-        expect(page).to have_selector(".city-details-review")
-        expect(page).to have_content("Visitor Reviews")
+        within ('.column-list') do
+          expect(page).to have_selector(".add-attraction-button")
+          expect(page).to have_selector(".attraction-details")
+          expect(page).to have_selector(".attraction-details-img")
+          expect(page).to have_selector(".attraction-details-name")
+          expect(page).to have_selector(".attraction-details-description")
+          expect(page).to have_selector(".attraction-details-wiki")
+          expect(page).to have_content("Wikipedia Article")
+          expect(page).to have_selector(".attraction-details-website")
+          expect(page).to have_content("Official Website")
+          expect(page).to have_selector(".attraction-details-admission")
+          expect(page).to have_content("Admission")
+          expect(page).to have_selector(".attraction-details-hours")
+          expect(page).to have_content("Opening Hours")
+          expect(page).to have_selector(".attraction-details-address")
+          expect(page).to have_content("Address")
+          expect(page).to have_selector(".attraction-details-phone")
+          expect(page).to have_selector(".attraction-details-email")
+          expect(page).to have_selector(".attraction-details-review")
+          expect(page).to have_content("Visitor Reviews")
 
-        # expect(page).to have_content("Sign in or create an account to leave a review")
-        # expect(page).to have_selector(".city-details-tags")
-        # Tags are an extension feature
+          # expect(page).to have_content("Sign in or create an account to leave a review")
+          # expect(page).to have_selector(".city-details-tags")
+          # Tags are an extension feature
+        end
       end
     end
   end
