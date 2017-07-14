5763495878fcd8b1d6c58ca7bc227d45aa84c0d9
NataliaColome <nmcolome@gmail.com>
2017-07-13 19:07:51 -0600

    add sad path testing and functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:session][:email])
-    if @user && user.authenticate(params[:session][:password])
+    if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       redirect_to user_trips_path(@user)
     else
--- spec/features/user_with_saved_trips_signs_in_spec.rb
+++ spec/features/user_with_saved_trips_signs_in_spec.rb
@@ -18,12 +18,33 @@ feature "user signs in" do
       fill_in "Password", with: "password"
       find(".btn-sign-in").click
 
-      # And I am redirected to a prompt page asking to make a new trip or add to
-      # existing trip
-      # What path should this be? A partial on the display trips page?
-
+      expect(current_path).to eq user_trips_path(user)
       expect(page).to have_content("Create New Trip")
       expect(page).to have_content("Choose Existing Trip")
+    end
+
+    it "they enter wrong email and see a flash message" do
+      user = create(:user_with_trips)
+
+      visit login_path
+      fill_in "Email", with: "wrong@email.com"
+      fill_in "Password", with: "password"
+      find(".btn-sign-in").click
+
+      expect(current_path).to eq login_path
+      expect(page).to have_content("The email or password you entered is incorrect")
+    end
+
+    it "they enter wrong password and see a flash message" do
+      user = create(:user_with_trips)
+
+      visit login_path
+      fill_in "Email", with: user.email
+      fill_in "Password", with: "wrong"
+      find(".btn-sign-in").click
+
+      expect(current_path).to eq login_path
+      expect(page).to have_content("The email or password you entered is incorrect")
     end
   end
 end
