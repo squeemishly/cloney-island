a5aa55c990f5368c97377702b6333e28e0022086
Sarah Kirk <kirk710@gmail.com>
2017-07-14 10:30:47 -0600

    finishes user story 17 for feature test, need to test for JS

---
+++ spec/features/users/logged_in_user_can_see_path_for_journey_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "logged in user" do
+  it "can see path of their journey for the day" do
+    before(:each) do
+      allow_any_instance_of(ApplicationController).to recieve(current_user).and_return(user)
+    end
+    user = create(:user)
+    attraction = create(:attraction)
+    user.attractions << attraction
+    visit attraction_path(attraction.id)
+
+    #javascript test for seeing trail on map
+
+# 17. Logged in user can see the path of their journey for the day
+# As a logged in user,
+# When I add a place to my Daily Plan
+# Then I should see a trail on the map from place to place
