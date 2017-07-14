26092b42ce327937006f5cf6f2ab18c5a30ffdf1
Sarah Kirk <kirk710@gmail.com>
2017-07-14 10:49:40 -0600

    adds feature test 20

---
+++ spec/features/users/logged_in_user_sees_options_with_nav_bar_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "logged in user" do
+  it "sees options with nav-bar" do
+    before(:each) do
+      allow_any_instance_of(ApplicationController).to recieve(current_user).and_return(user)
+    end
+
+    visit root_path
+    expect(page).to have_selector(".user-avatar")
+    page.find(".user-avatar").click
+    expect(page).to have_selector(".user-dropdown")
+    expect(page).to have_content("Plan a new Trip")
+    expect(page).to have_content("My Trips")
+    expect(page).to have_content("My Account")
+    expect(page).to have_content("Sign Out")
+  end
+end
+
+# 20. Logged In User Sees Options with Personal Nav-Bar
+# As a logged in user
+# I see my avatar in the top right corner
+# And when I click on my avatar
+# I see a dropdown menu
+# And I expect the drop down menu to include a link for “Plan a new trip”
+# And I expect the drop down menu to include a link for “My Trips”
+# And I expect the drop down menu to include a link for “My Account”
+# And I expect the drop down menu to include a link for “Sign Out”
