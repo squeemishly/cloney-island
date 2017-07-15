5980bf853558a69b8d2bdd434cf9358ec3e2eb2b
NataliaColome <nmcolome@gmail.com>
2017-07-15 14:19:36 -0600

    add attraction details to Attraction PORO, refactor views to call Attraction methods (name, photo_url...)

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -2,18 +2,36 @@ class Attraction
   attr_reader :name,
               :vicinity,
               :place_id,
-              :photo_reference,
+              :photo_url,
               :lat,
-              :lng
+              :lng,
+              :formatted_address,
+              :formatted_phone_number,
+              :rating,
+              :website,
+              :opening_hours,
+              :international_phone_number,
+              :reviews
 
   def initialize(attr={})
     @attr = attr
     @name = attr[:name]
     @vicinity = attr[:vicinity]
     @place_id = attr[:place_id]
-    @photo_reference = attr[:photos][0][:photo_reference] if attr[:photos]
     @lat = attr[:geometry][:location][:lat]
     @lng = attr[:geometry][:location][:lng]
+    if attr[:photos]
+      @photo_url = "https://maps.googleapis.com/maps/api/place/photo?photoreference=#{attr[:photos][0][:photo_reference]}&sensor=false&maxheight=300&maxwidth=300&key=#{ENV['google_map_api_key']}"
+    else
+      @photo_url = "http://www.slepamapask.eu/img/slepa_mapa.png"
+    end
+    @formatted_address = attr[:formatted_address] if attr[:formatted_address]
+    @formatted_phone_number = attr[:formatted_phone_number] if attr[:formatted_phone_number]
+    @rating = attr[:rating] if attr[:rating]
+    @website = attr[:website] if attr[:website]
+    @opening_hours = attr[:opening_hours] if attr[:opening_hours]
+    @international_phone_number = attr[:international_phone_number] if attr[:international_phone_number]
+    @reviews = attr[:reviews] if attr[:reviews]
   end
 
   def self.fetch_attractions_by_city(city)
@@ -30,8 +48,7 @@ class Attraction
   end
 
   def self.fetch_details(place_id)
-    # GooglePlacesService.fetch_details(place_id)
-    
+    new(GooglePlacesService.fetch_details(place_id))
   end
 
   private
--- spec/models/attraction_spec.rb
+++ spec/models/attraction_spec.rb
@@ -13,7 +13,8 @@ describe Attraction do
               "name": "Eiffel Tower",
               "photos": [
                   {
-                      "photo_reference": "CmRaAAAAkYtmO5R9m4docIuo-HGALNkkor4tECv68NKqWGJMU7Fbbgahpf6YLZCrmBUexLtLvzW_6IDH5bXboceO9hTIdEW1vH_SB538SHRX_hUF_x8XjWYtFZstD4kuPVzrE0qFEhD0ZslHLhgJcy--UBwst5tEGhTm4TjufhB0gmUNXIJM-rIcrWuxLQ",
+                      "photo_reference": 
+                      "CmRaAAAAkYtmO5R9m4docIuo-HGALNkkor4tECv68NKqWGJMU7Fbbgahpf6YLZCrmBUexLtLvzW_6IDH5bXboceO9hTIdEW1vH_SB538SHRX_hUF_x8XjWYtFZstD4kuPVzrE0qFEhD0ZslHLhgJcy--UBwst5tEGhTm4TjufhB0gmUNXIJM-rIcrWuxLQ",
                   }
               ],
               "place_id": "ChIJLU7jZClu5kcR4PcOOO6p3I0",
@@ -25,7 +26,7 @@ describe Attraction do
       expect(attraction.name).to eq("Eiffel Tower")
       expect(attraction.vicinity).to eq("Champ de Mars, 5 Avenue Anatole France, Paris")
       expect(attraction.place_id).to eq("ChIJLU7jZClu5kcR4PcOOO6p3I0")
-      expect(attraction.photo_reference).to eq("CmRaAAAAkYtmO5R9m4docIuo-HGALNkkor4tECv68NKqWGJMU7Fbbgahpf6YLZCrmBUexLtLvzW_6IDH5bXboceO9hTIdEW1vH_SB538SHRX_hUF_x8XjWYtFZstD4kuPVzrE0qFEhD0ZslHLhgJcy--UBwst5tEGhTm4TjufhB0gmUNXIJM-rIcrWuxLQ")
+      expect(attraction.photo_url).to eq("https://maps.googleapis.com/maps/api/place/photo?photoreference=CmRaAAAAkYtmO5R9m4docIuo-HGALNkkor4tECv68NKqWGJMU7Fbbgahpf6YLZCrmBUexLtLvzW_6IDH5bXboceO9hTIdEW1vH_SB538SHRX_hUF_x8XjWYtFZstD4kuPVzrE0qFEhD0ZslHLhgJcy--UBwst5tEGhTm4TjufhB0gmUNXIJM-rIcrWuxLQ&sensor=false&maxheight=300&maxwidth=300&key=#{ENV['google_map_api_key']}")
       expect(attraction.lat).to eq(48.85837009999999)
       expect(attraction.lng).to eq(2.2944813)
     end
@@ -50,7 +51,7 @@ describe Attraction do
       expect(attraction.name).to eq("Eiffel Tower")
       expect(attraction.vicinity).to eq("Champ de Mars, 5 Avenue Anatole France, Paris")
       expect(attraction.place_id).to eq("ChIJLU7jZClu5kcR4PcOOO6p3I0")
-      expect(attraction.photo_reference).to eq(nil)
+      expect(attraction.photo_url).to eq("http://www.slepamapask.eu/img/slepa_mapa.png")
       expect(attraction.lat).to eq(48.85837009999999)
       expect(attraction.lng).to eq(2.2944813)
     end
