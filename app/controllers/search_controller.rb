175c07b711f9a88be28fa2e79ae5b5a334d56f4b
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-14 18:34:11 -0600

    Add markers to map

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,11 @@ class SearchController < ApplicationController
       @city = eval(params[:city])
       response = Faraday.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{@city[:lat]},#{@city[:lng]}&radius=5000&types=point-of-interest&keyword=attraction&language=english&key=#{ENV['google_map_api_key']}")
       @attractions = JSON.parse(response.body, symbolize_names: true)[:results]
+      @attractions_js = []
+      @attractions.each do |at_hash|
+        @attractions_js << [at_hash[:name].to_s, at_hash[:geometry][:location][:lat], at_hash[:geometry][:location][:lng]]
+      end
+      @attractions_js
     elsif params[:attraction]
       response = Faraday.get("https://maps.googleapis.com/maps/api/place/details/json?placeid=#{params[:attraction]}&key=#{ENV['google_map_api_key']}")
       @attraction = JSON.parse(response.body, symbolize_names: true)[:result]
