fe2369202fa4aa3bbae3dfcf4e61a37c9328fc17
squeemishly <squeemishly@gmail.com>
2017-07-16 13:17:35 -0600

    a user with no saved trips can create a trip at login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,11 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to user_trips_path(@user)
+      if @user.trips == []
+        redirect_to new_user_trip_path(@user)
+      else
+        redirect_to user_trips_path(@user)
+      end
     else
       flash[:notice] = "The email or password you entered is incorrect"
       redirect_to login_path
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,8 +1,27 @@
 class TripsController < ApplicationController
   def new
+    @user = current_user
+    @trip = Trip.new
   end
 
   def index
-    
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @trip = Trip.new(trip_params)
+    @user.trips << @trip
+    if @trip.save
+      redirect_to '/'
+    else
+      @trip = Trip.new
+      redirect_to new_user_trip_path(@user)
+    end
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:start_date, :end_date, :start_city)
   end
 end
