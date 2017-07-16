f50782be200c33f5c5aa150f2422b8b5db55b98e
squeemishly <squeemishly@gmail.com>
2017-07-16 17:50:07 -0600

    validates appropriate fields of trip and changes test to test for user singular

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,8 @@
 class Trip < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :start_city
+  validates_presence_of :start_date
+  validates_presence_of :end_date
+  validates_presence_of :user_id
 end
