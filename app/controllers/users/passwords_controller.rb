a4c06b6df87991d24df9b7ddf4595011c167254c
rwilhel <rwilhel@gmail.com>
2017-07-16 15:35:24 -0600

    Implements password confirmation controller action for updating password with valid params and not updating with invalid params

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -10,7 +10,29 @@ class Users::PasswordsController < ApplicationController
     @user = current_user
   end
 
+  def update
+    if passwords_not_empty? && passwords_equal?
+      current_user.update(password_params)
+      redirect_to user_trips_path(current_user.id), success: "Password Updated"
+      sessions[:authenticated] = false
+    else
+      redirect_to users_password_edit_path(current_user.id), warning: "Error, please try again."
+    end
+  end
+
   private
+
+    def password_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    def passwords_not_empty?
+      params[:user][:password].length > 0 && params[:user][:password_confirmation].length > 0
+    end
+
+    def passwords_equal?
+      params[:user][:password] == params[:user][:password_confirmation]
+    end
 
     def authenticated?
       render file: "#{Rails.root}/public/404.html", status: 404 unless session[:authenticated]
