61b327eec7cbccc60e7b11e87d75bbfc5198a44c
squeemishly <squeemishly@gmail.com>
2017-07-17 15:56:44 -0600

    started to fix test to allow current_user

--- spec/features/logged_in_user_sees_navigation_options_spec.rb
+++ spec/features/logged_in_user_sees_navigation_options_spec.rb
@@ -1,17 +1,15 @@
 # require 'rails_helper'
-
+#
 # RSpec.describe "a logged in user" do
 #   it "sees navigation options" do
-#     before(:each) do
-#      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-#    end
 #    user = create(:user, password: "password")
+#    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 #    places = create_list(:place, 10)
 #    place1 = place.first
 #    date = Date.today
-
+#
 #    visit user_dashboard_path(user.id)
-
+#
 #    click_on "Create New Trip"
 #    expect(page).to have_content("Current Day")
 #    expect(page).to have_button("Daily Plan")
