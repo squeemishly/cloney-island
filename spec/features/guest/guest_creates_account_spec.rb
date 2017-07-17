0ca62edfa5f3a45a484b5db826bf1f9446d5e285
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-17 07:22:50 -0600

    Create add-attraction-button placeholder for single attraction

--- spec/features/guest/guest_creates_account_spec.rb
+++ spec/features/guest/guest_creates_account_spec.rb
@@ -1,47 +1,47 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# feature "guest creates account" do
-#   context "they fill in form with valid credentials" do
-#     it "they see the new trip form" do
+feature "guest creates account" do
+  context "they fill in form with valid credentials" do
+    it "they see the new trip form" do
 
-#       visit login_path
-#       click_on "Create an Account with Email"
+      visit login_path
+      click_on "Create an Account with Email"
 
-#       expect(current_path).to eq(new_user_path)
-#       expect(User.count).to eq(0)
+      expect(current_path).to eq(new_user_path)
+      expect(User.count).to eq(0)
 
-#       fill_in "First name", with: "Jimbo"
-#       fill_in "Last name", with: "Tron"
-#       fill_in "Email", with: "jimbotron@example.com"
-#       fill_in "Password", with: "password"
-#       fill_in "Confirm Password", with: "password"
-#       fill_in "Phone", with: "303-555-5555"
-#       click_on "Sign Up"
+      fill_in "First name", with: "Jimbo"
+      fill_in "Last name", with: "Tron"
+      fill_in "Email", with: "jimbotron@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Confirm Password", with: "password"
+      fill_in "Phone", with: "303-555-5555"
+      click_on "Sign Up"
 
-#       expect(User.count).to eq(1)
-#       expect(current_path).to eq(new_user_trip_path(User.first))
-#     end
-#   end
+      expect(User.count).to eq(1)
+      expect(current_path).to eq(new_user_trip_path(User.first))
+    end
+  end
 
-#   context "they fill in form with invalid credentials" do
-#     it "they see the new user form and a flash message" do
-#       visit login_path
-#       click_on "Create an Account with Email"
+  context "they fill in form with invalid credentials" do
+    it "they see the new user form and a flash message" do
+      visit login_path
+      click_on "Create an Account with Email"
 
-#       expect(current_path).to eq(new_user_path)
-#       expect(User.count).to eq(0)
+      expect(current_path).to eq(new_user_path)
+      expect(User.count).to eq(0)
 
-#       fill_in "First name", with: "Jimbo"
-#       fill_in "Last name", with: "Tron"
-#       fill_in "Email", with: "jimbotron@example.com"
-#       fill_in "Password", with: "password"
-#       fill_in "Confirm Password", with: "wrong"
-#       fill_in "Phone", with: "303-555-5555"
-#       click_on "Sign Up"
+      fill_in "First name", with: "Jimbo"
+      fill_in "Last name", with: "Tron"
+      fill_in "Email", with: "jimbotron@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Confirm Password", with: "wrong"
+      fill_in "Phone", with: "303-555-5555"
+      click_on "Sign Up"
 
-#       expect(User.count).to eq(0)
-#       expect(current_path).to eq(new_user_path)
-#       expect(page).to have_content("User creation failed. Please try again")
-#     end
-#   end
-# end
+      expect(User.count).to eq(0)
+      expect(current_path).to eq(new_user_path)
+      expect(page).to have_content("User creation failed. Please try again")
+    end
+  end
+end
