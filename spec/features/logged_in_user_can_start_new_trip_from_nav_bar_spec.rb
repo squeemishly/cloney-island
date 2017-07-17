91d8f6df1005411aec6dc7c5c1d6d02a2316e5ac
squeemishly <squeemishly@gmail.com>
2017-07-17 16:44:58 -0600

    user can start a new trip from the navbar. a guest must log in first.

--- spec/features/logged_in_user_can_start_new_trip_from_nav_bar_spec.rb
+++ spec/features/logged_in_user_can_start_new_trip_from_nav_bar_spec.rb
@@ -1,19 +1,38 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.feature "logged in user" do
-#   it "sees options with nav-bar" do
-#     before(:each) do
-#       allow_any_instance_of(ApplicationController).to recieve(current_user).and_return(user)
-#     end
+RSpec.feature "Create a New Trip" do
+  context "a logged in user" do
+    it "from their avatar" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-#     visit root_path
-#     visit root_path
-#     expect(page).to have_selector(".user-avatar")
-#     page.find(".user-avatar").click
-#     page.find("Plan New Trip").click
-#     expect(current_path).to eq(new_trips_path)
-#   end
-# end
+      visit root_path
+
+      page.find(".user-avatar").click
+      click_on "Plan New Trip"
+      expect(current_path).to eq(new_user_trip_path(user))
+    end
+
+    it "from the nav bar" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit root_path
+
+      click_on "New Trip"
+      expect(current_path).to eq(new_user_trip_path(user))
+    end
+  end
+
+  context "a guest" do
+    it "must login first" do
+      visit root_path
+
+      click_on "New Trip"
+      expect(current_path).to eq login_path
+    end
+  end
+end
 
 # # 21. Logged In User Can Start New Trip in Nav Bar
 # # As a logged in user
