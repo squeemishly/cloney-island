34d922fabfe0599e1634a2dde738ba439efc82fe
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-17 18:21:52 -0600

    Add validations to tour.rb

--- spec/models/tour_spec.rb
+++ spec/models/tour_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe Tour, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:price) }
-    it { should validate_presence_of(:avg_rating) }
     it { should validate_presence_of(:user_id) }
   end
 
