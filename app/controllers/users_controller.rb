8b49170dd8b7340fe3a79fce8e5bc100067681ca
NataliaColome <nmcolome@gmail.com>
2017-07-17 21:33:38 -0600

    add functionality for user to them to delete their accout: create user#destroy route and action, and add flash message to confirm action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(current_user.id)
+    session.clear
+    @user.destroy
+    redirect_to root_path
+  end
+
   private
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :phone, :password, :status, :role, :password_confirmation)
