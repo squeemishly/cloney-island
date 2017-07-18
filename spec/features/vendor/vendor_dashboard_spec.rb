4523679fddd0c31f0f9ff84e2889a80bb1ce750f
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-17 20:05:44 -0600

    Create user_with_tours factory

--- spec/features/vendor/vendor_dashboard_spec.rb
+++ spec/features/vendor/vendor_dashboard_spec.rb
@@ -1,42 +1,40 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "Vendor" do
-#   before :each do
-#     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(vendor)
-#     vendor = create(:user_with_tours)
-#     role = create(:role, name: "vendor")
-#     vendor.roles << role
-#     tour1 = vendor.tours.first
-#     # tour1 = create(:tour_with_rating) #??
-#     # vendor.tours << tour1
-#   end
+RSpec.describe "Vendor" do
+  before :each do
+    vendor = create(:user_with_tours, role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(vendor)
+    tour1 = vendor.tours.first
+    # tour1 = create(:tour_with_rating) #??
+    # vendor.tours << tour1
+  end
 
-#   it "sees their dashboard" do
-#     visit "/vendor/#{vendor.id}/dashboard"
+  it "sees their dashboard" do
+    visit "/vendor/#{vendor.id}/dashboard"
 
-#     expect(page).to have_selector('.tour', count: vendor.tours.count)
-#     expect(page).to have_content(tour1.name)
-#     expect(page).to have_content(tour1.price)
-#     expect(page).to have_content(tour1.avg_rating)
-#     expect(page).to have_content("Edit")
-#     expect(page).to have_content("Delete")
-#     expect(page).to have_content("New Tour")
-#   end
+    expect(page).to have_selector('.tour', count: vendor.tours.count)
+    expect(page).to have_content(tour1.name)
+    expect(page).to have_content(tour1.price)
+    expect(page).to have_content(tour1.avg_rating)
+    expect(page).to have_content("Edit")
+    expect(page).to have_content("Delete")
+    expect(page).to have_content("New Tour")
+  end
 
-#   it "can't see other vendor's dashboard" do
-#     vendor2 = create(:user)
-#     role = create(:role, name: "vendor")
-#     vendor2.roles << role
+  xit "can't see other vendor's dashboard" do
+    vendor2 = create(:user)
+    role = create(:role, name: "vendor")
+    vendor2.roles << role
 
-#     visit "/vendor/#{vendor2.id}/dashboard"
+    visit "/vendor/#{vendor2.id}/dashboard"
 
-#     expect(page).to have_content "The page you were looking for doesn't exist."
-#   end
-# end
+    expect(page).to have_content "The page you were looking for doesn't exist."
+  end
+end
 
-# # As a vendor
-# # When I visit vendor/dashboard
-# # I see all my tours
-# # I see an edit button for each tour
-# # I see a delete button for each tour
-# # I see a “new tour” button
+# As a vendor
+# When I visit vendor/dashboard
+# I see all my tours
+# I see an edit button for each tour
+# I see a delete button for each tour
+# I see a “new tour” button
