57141234509a12152f5952b099c1578e05db243e
Sarah Kirk <kirk710@gmail.com>
2017-07-17 20:17:20 -0600

    Functioning seed file up and running

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 class Seed
   def self.start
+    seed = Seed.new
     seed.generate_users
+    seed.generate_trips
   end
 
   def generate_users
@@ -8,18 +10,34 @@ class Seed
       user = User.create!(
             first_name: Faker::Zelda.character,
             last_name: Faker::Zelda.character,
-            email: Faker::Internet.email)
-      puts "User #{i}: #{user.name} - #{user.email} created!"
+            email: Faker::Internet.email,
+            phone: Faker::PhoneNumber.phone_number,
+            password: "password",
+            role: 0,
+            verification_code: nil,
+            )
+      puts "User #{i}: #{user.first_name} created!"
     end
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone"
-    t.string "password_digest"
-    t.integer "status", default: 0
-    t.integer "role"
-    t.string "verification_code"
+  def generate_trips
+    2000.times do |i|
+      user = User.find(Random.new.rand(1..1000))
+      trip = user.trips.create!(
+            start_date: "2017-11-17",
+            end_date: "2017-11-18",
+            start_city: Faker::Address.city,
+      )
+      add_trips(user, trip)
+      puts "Trip #{i}: Trip for #{user.first_name} created!"
+    end
   end
+
+  private
+    def add_trips(user, trip)
+      user.trips << trip
+      puts "Added trip #{trip.id} to user #{user.id}."
+    end
+end
+
+Seed.start
