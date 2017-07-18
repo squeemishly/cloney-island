f13428bbac69c8c26e4973e04813028867ab9d77
squeemishly <squeemishly@gmail.com>
2017-07-17 19:45:33 -0600

    when user creates a trip, they are taken to the city of the trip and can see attractions on the left

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -9,11 +9,16 @@ class TripsController < ApplicationController
   end
 
   def create
+    response = Faraday.get("https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyDsH3cXjVtbV0tw7CcDAbDQQEt39K7MCyA&query=#{params[:trip][:start_city]}")
+    raw_city_info = JSON.parse(response.body, symbolize_names: true)[:results][0]
+    @city = City.create_new_city(raw_city_info)
     @user = User.find(params[:user_id])
     @trip = Trip.new(trip_params)
     @user.trips << @trip
+    @city.trips << @trip
     if @trip.save
-      redirect_to '/'
+      city_place_info = {name: @city.name, lat: @city.lat, lng: @city.lng}
+      redirect_to "/search?city=#{city_place_info}"
     else
       @trip = Trip.new
       redirect_to new_user_trip_path(@user)
---
+++ app/models/city.rb
@@ -0,0 +1,12 @@
+class City < ApplicationRecord
+  has_many :trips
+  
+  def self.create_new_city(args)
+    name = args[:name]
+    lat = args[:geometry][:location][:lat]
+    lng = args[:geometry][:location][:lng]
+    place_id = args[:place_id]
+    City.create(name: name, lat: lat, lng: lng, place_id: place_id)
+  end
+
+end
