9859f5894f850e331acfd33d5a76799a1f29a10a
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-18 20:21:58 -0600

    Delete fixtures

--- spec/features/user/logged_in_user_sees_navigation_options_spec.rb
+++ spec/features/user/logged_in_user_sees_navigation_options_spec.rb
@@ -9,9 +9,9 @@
  #    places = create_list(:place, 10)
  #    place1 = place.first
  #    date = Date.today
- -
+ 
  #    visit user_dashboard_path(user.id)
- -
+ 
  #    click_on "Create New Trip"
  #    expect(page).to have_content("Current Day")
  #    expect(page).to have_button("Daily Plan")
