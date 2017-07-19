d21cc147026e713f2399c5e8ab35ba812f5a01d6
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-19 07:40:48 -0600

    Create tour index and related navs

--- spec/features/vendor/vendor_accesses_tours_index_spec.rb
+++ spec/features/vendor/vendor_accesses_tours_index_spec.rb
@@ -4,7 +4,7 @@ feature "Vendor accesses all tours" do
   before :each do
     @vendor = create(:user_with_tours, role: 1)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@vendor)
-    tour1 = @vendor.tours.first
+    @tour1 = @vendor.tours.first
   end
 
   context "click on view all tours from dashboard" do
@@ -13,21 +13,22 @@ feature "Vendor accesses all tours" do
       click_on "View All Tours"
 
       expect(page).to have_selector('.tour', count: @vendor.tours.count)
-      expect(page).to have_content(tour1.name)
-      expect(page).to have_content(tour1.price)
-      expect(page).to have_content(tour1.avg_rating)
+      expect(page).to have_content(@tour1.name)
+      expect(page).to have_content(@tour1.price)
+      expect(page).to have_content(@tour1.average_rating)
     end
   end
 
-    context "click on tours on navbar" do
+  context "click on tours on navbar" do
     it "see their tours" do
       visit user_vendor_dashboard_path(@vendor)
       find('.user-avatar').click
       find('.my-tours').click
 
       expect(page).to have_selector('.tour', count: @vendor.tours.count)
-      expect(page).to have_content(tour1.name)
-      expect(page).to have_content(tour1.price)
-      expect(page).to have_content(tour1.avg_rating)
+      expect(page).to have_content(@tour1.name)
+      expect(page).to have_content(@tour1.price)
+      expect(page).to have_content(@tour1.average_rating)
     end
   end
+end
