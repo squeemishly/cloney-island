9d57db91c7a681fbadd15c08b3ed93fe24573189
rwilhel <rwilhel@gmail.com>
2017-07-18 22:52:18 -0600

    Finishes spec for user_can_see_account_options_spec

--- spec/features/user/user_can_see_account_options_spec.rb
+++ spec/features/user/user_can_see_account_options_spec.rb
@@ -1,36 +1,22 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "A signed-in user can view their account options in My Account" do
-#   it "can view their account options" do
-#     user = create(:user)
+RSpec.describe "A signed-in user can view their account options in My Account" do
+  it "can view their account options" do
+    user = create(:user)
 
-#     visit root_path
+    visit root_path
 
-#     click_on 'Sign In'
-#     fill_in 'Email', with: "#{user.email}"
-#     fill_in 'Password', with: 'password'
-#     find(".btn-sign-in").click
-#     # select('My Account', :from => 'Select Box')
+    click_on 'Sign In'
+    fill_in 'Email', with: "#{user.email}"
+    fill_in 'Password', with: 'password'
+    find(".btn-sign-in").click
+    click_on "My Account"
 
-#     expect(current_path).to eq("/users/#{user.id}/trips")
-#     expect(page).to have_content("Edit Email")
-#     expect(page).to have_content("Edit Phone")
-#     expect(page).to have_content("Update Image")
-#     expect(page).to have_content("Change Password")
-#     expect(page).to have_content("View My Reviews")
-#     expect(page).to have_content("Delete Account")
-#   end
-# end
-
-# # 28. A logged in user can see account options in My Account
-# # **Assuming not using oauth**
-# # As a logged in user
-# # When I visit my Account
-# # Then I will see...
-# # -edit email
-# # -edit phone
-# # -update image
-# # -change password
-# # -View my reviews
-# # -delete account
-# # *revisit for oauth parameters
+    expect(current_path).to eq("/users/#{user.id}")
+    expect(page).to have_content("Edit Email")
+    expect(page).to have_content("Edit Phone")
+    expect(page).to have_content("Change Password")
+    expect(page).to have_content("View My Reviews")
+    expect(page).to have_content("Delete Account")
+  end
+end
