1e613501c6f7ed5c38475a352021562ac07ac100
Sarah Kirk <kirk710@gmail.com>
2017-07-19 14:37:09 -0600

    ensures that filter navbar only shows up after clicking on an attraction, logged in user selects test passes

--- spec/features/user/logged_in_user_selected_city_sees_filters_spec.rb
+++ spec/features/user/logged_in_user_selected_city_sees_filters_spec.rb
@@ -3,27 +3,20 @@ require 'rails_helper'
 feature "guest visits root" do
   before(:each) do
     city = create(:city)
+    city = create(:city, name: "London")
   end
   context "they are not logged in" do
     it "they see main page elements" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       VCR.use_cassette("main_page_elements") do
         visit root_path
-        within ('.column-list') do
-          find(".attraction-preview").click
+
+        within (".column-list") do
+          first("a").click
         end
 
-        expect(page).to have_selector(".logo")
-        within('.navbar-main') do
-          expect(page).to have_content('Sign In')
-          expect(page).to have_content('New Trip')
-        end
-
-        within('.navbar-secondary') do
-          expect(page).to have_content('Attractions')
-          expect(page).to have_content('Tours')
-        end
-
-        within('.navbar-filters') do
+        within('.navbar-collapse.navbar-filters') do
           expect(page).to have_content('Sightseeing')
           expect(page).to have_content('Shopping')
           expect(page).to have_content('Restaurants')
@@ -38,25 +31,6 @@ feature "guest visits root" do
 
         expect(page).to have_selector(".main-map")
         expect(page).to have_selector(".column-list")
-      end
-    end
-  end
-
-  context "they click on a city" do
-    it "they see the display page for the city they clicked on" do
-      VCR.use_cassette("display_page_for_city") do
-        visit root_path
-
-        within ('.column-list') do
-          expect(page).to have_selector(".city-preview")
-          click_on "Paris, France"
-        end
-
-        within(".column-list") do
-          expect(page).to have_content("Paris")
-          expect(page).to_not have_content("London")
-          expect(page).to have_selector(".attraction-preview")
-        end
       end
     end
   end
