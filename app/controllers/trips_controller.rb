ae179ffd8b6a3e4d3a9a3a2a409fb3885d5cec5f
squeemishly <squeemishly@gmail.com>
2017-07-19 15:36:06 -0600

    updates cassettes

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -39,7 +39,7 @@ class TripsController < ApplicationController
       place.lng = params[:lng]
     end
     @trip.itineraries.create(place_id: @place.id, date: @trip.start_date)
-    flash.now[:notice] = "#{@place.name} successfully added"
+    flash.now[:alert] = "#{@place.name} successfully added"
   end
 
   private
