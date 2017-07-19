fb5d5f7393afd171a0cdd9e18313330bf1304931
NataliaColome <nmcolome@gmail.com>
2017-07-19 13:23:12 -0600

    modify test to reflect real functionality, create query#index route, controller, method and view

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
 namespace :admin do
   get '/dashboard', to: 'dashboard#index'
+  get '/query', to: 'query#index', as: 'query'
 end
 
   namespace :users, path: ":id" do
