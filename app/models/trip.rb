9431f881898b5ebdec30702f8d43029a10cfe7f6
squeemishly <squeemishly@gmail.com>
2017-07-19 19:38:40 -0600

    started jquery for responsive dropdown menus, added date dropdown menu to navbar

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -8,4 +8,12 @@ class Trip < ApplicationRecord
   validates_presence_of :start_date
   validates_presence_of :end_date
   validates_presence_of :user_id
+
+  def with_dates
+    "#{start_city}, #{start_date} - #{end_date}"
+  end
+
+  def days
+    (start_date..end_date).to_a
+  end
 end
