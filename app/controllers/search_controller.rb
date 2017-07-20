1b8d600d74189ec7a4ef7d8c918a779b836976f7
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-19 20:22:40 -0600

    Remove apostrophes from attraction.name before passing to application_js in application.rb

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,7 +8,6 @@ class SearchController < ApplicationController
       end
 
       @attraction_type = params[:attraction_type] || "point-of-interest"
-
       @attractions = Attraction.fetch_attractions_by_city(@attraction_type, @city)
       @attractions_js = Attraction.marker_format(@attractions)
     elsif params[:attraction]
