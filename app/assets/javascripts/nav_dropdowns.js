4a95c5197a0160608f3e34dae94f91bd77e9ca51
squeemishly <squeemishly@gmail.com>
2017-07-19 20:43:35 -0600

    ajaxified the splines

--- app/assets/javascripts/nav_dropdowns.js
+++ app/assets/javascripts/nav_dropdowns.js
@@ -1,10 +1,13 @@
 $(document).on('turbolinks:load', function() {
   $("a.add-attraction-button").on("click", function(event) {
-    let tripId = $("select#trips").val();
-    let day = $("select#days").val();
-    let url = window.location.href.split('?')[0];
-    let newUrl = url + `?trip=${tripId}&day=${day}`;
-    //window.location = newUrl;
+    let selectedDay = $("select#days").val();
+    $.ajax({
+      url: newUrl,
+      data: { day: selectedDay },
+      method: "PUT"
+    }).done(function(response) {
+      alert("response");
+    });
     event.preventDefault();
   });
 });
