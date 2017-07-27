c6e17a0779e2c23fb200de266a8de373b0008de2
squeemishly <squeemishly@gmail.com>
2017-07-26 19:57:23 -0600

    search bar refactored

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,10 +13,12 @@ class SearchController < ApplicationController
     elsif params[:attraction]
       @attraction = Attraction.fetch_details(params[:attraction])
     elsif params[:query]
-      query = params[:query]
-      response = Faraday.get("https://maps.googleapis.com/maps/api/place/textsearch/json?key=#{ENV['google_map_api_key']}&query=#{query}")
-      info = JSON.parse(response.body, symbolize_names: true)[:results][0]
-      @city = City.city_search(info)
+      @city = City.city_find(params[:query])
+
+      # query = params[:query]
+      # response = Faraday.get("https://maps.googleapis.com/maps/api/place/textsearch/json?key=#{ENV['google_map_api_key']}&query=#{query}")
+      # info = JSON.parse(response.body, symbolize_names: true)[:results][0]
+      # @city = City.city_search(info)
 
       @attraction_type = "point-of-interest"
       @attractions = Attraction.fetch_attractions_by_city(@attraction_type, @city)
