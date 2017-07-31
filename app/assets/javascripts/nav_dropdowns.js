afe199e3168a690c1c753affd1decce37f1e7b52
squeemishly <squeemishly@gmail.com>
2017-07-30 21:38:09 -0600

    removed trips dropdown menu, troubleshooting date dropdown menu

--- app/assets/javascripts/nav_dropdowns.js
+++ app/assets/javascripts/nav_dropdowns.js
@@ -1,15 +1,21 @@
 $(document).on('turbolinks:load', function() {
+  // console.log("hello")
   $("a.add-attraction-button").on("click", function(event) {
     let newUrl = $(this).attr("href");
     let selectedDay = $("select#days").val();
+    // console.log(selectedDay)
+    // console.log(newUrl)
+
     $.ajax({
       url: newUrl,
       data: { day: selectedDay },
       method: "PUT"
     }).done(function(response) {
-      alert(response.responseText);
+      // alert(response.responseText);
+      alert(response);
     }).fail(function(response) {
-      alert(response.responseText);
+      // alert(response.responseText);
+      alert(response);
     });
     event.preventDefault();
   });
