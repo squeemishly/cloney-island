c2c7909e9a9a22f55dc3e75c4e0677bca11be243
NataliaColome <nmcolome@gmail.com>
2017-08-16 00:43:21 -0600

    remove commented out line from routes

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,6 @@ end
 
   resources :private_chats, only: [:index, :new, :create, :show]
   resources :messages, only: [:create]
-  # get '/chat_query', to: 'chat_query#index', as: 'chat_query'
 
   mount ActionCable.server, at: '/cable'
 end
