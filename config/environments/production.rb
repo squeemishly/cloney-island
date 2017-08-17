a64400ee51276f254a0aa9a6d9703d34831e331b
squeemishly <squeemishly@gmail.com>
2017-08-17 13:23:11 -0600

    configured action cable for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://stark-waters-79297.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://stark-waters-79297.herokuapp.com', 'http://stark-waters-79297.herokuapp.com']
 end
